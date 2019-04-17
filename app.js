47634cb172b55e346e31b0a7f2aec3795ba9970e
Nando <mnandohidayat@gmail.com>
2019-04-18 00:38:58 +0700

    done

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
 const routeOptions = {
-  A: 'BDG'
+  A: ['B', 'D', 'G'],
+  B: ['A', 'E', 'F'],
+  C: ['F', 'H'],
+  D: ['A', 'F'],
+  E: ['B', 'G'],
+  F: ['B', 'C', 'D'],
+  G: ['A', 'E'],
+  H: ['C']
 }
+
+const main = () => {
+  const queue = ['A'],
+    steps = []
+  while (queue.length) {
+    steps.push(queue[0])
+    routeOptions[queue[0]].map(route => {
+      if (!queue.includes(route) && !steps.includes(route)) {
+        queue.push(route)
+      }
+    })
+    queue.shift()
+  }
+  console.log(steps)
+}
+
+main()
